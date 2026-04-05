0383df4f9965153f69c1b28e3cbc6d4420720f08
Ngoc Tran <163379246+ngxccc@users.noreply.github.com>
2026-04-05 13:17:40 +0000

    feat(auto_patch_config.sh): remove redundant kernel configuration commands feat(kernel_build_common.sh): define RESULT_DIR for output artifacts

--- scripts/auto_patch_config.sh
+++ scripts/auto_patch_config.sh
@@ -22,22 +22,7 @@ fi
 
 echo "[PATCH] Applying config modifications to $CONFIG_FILE..."
 
-# Enable KernelSU Core
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS
-
-# Enable SUSFS Features
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_SUS_PATH
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_SUS_MOUNT
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_SUS_KSTAT
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-./scripts/config --file "$CONFIG_FILE" --enable CONFIG_KSU_SUSFS_SPOOF_UNAME
-
-# Disable problematic features
-./scripts/config --file "$CONFIG_FILE" --disable CONFIG_KSU_SUSFS_SUS_OVERLAYFS
-./scripts/config --file "$CONFIG_FILE" --disable CONFIG_KSU_SUSFS_OPEN_REDIRECT
-./scripts/config --file "$CONFIG_FILE" --disable CONFIG_KSU_SUSFS_TRY_UMOUNT
+# ./scripts/config --file "$CONFIG_FILE" --enable CONFIG
+# ./scripts/config --file "$CONFIG_FILE" --disable CONFIG
 
 echo "[SUCCESS] Config patches applied to: $CONFIG_FILE"
